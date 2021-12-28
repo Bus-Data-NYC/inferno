6275afbd158b0dcff6c5ee0b68ec5cd0e01f1816
fitnr <fitnr@fakeisthenewreal>
2021-12-28 07:48:59 -0500

    fix off-by-one in run.sh

--- src/run.sh
+++ src/run.sh
@@ -5,6 +5,7 @@
 # --array-properties size=31 \
 # --container-overrides "environment=[{name=YEAR,value=2021},{name=MONTH,value=10}]"
 INFERNOFLAGS=${INFERNOFLAGS---quiet --incomplete}
-day=${DAY:-${AWS_BATCH_JOB_ARRAY_INDEX}}
+arrayday=$(date -d "${YEAR}-${MONTH}-01 + ${AWS_BATCH_JOB_ARRAY_INDEX} days" +"%d")
+day=${DAY:-$arrayday}
 DAY=$(echo ${day:-1} | xargs printf "%02.0f")
 ./src/inferno.py ${INFERNOFLAGS} ${YEAR}-${MONTH}-${DAY}
